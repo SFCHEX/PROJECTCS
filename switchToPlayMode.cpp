ee3d61e974582813f824fb68289bc1d3686b130b
amar298sherif <117904287+amar298sherif@users.noreply.github.com>
2022-11-25 20:59:09 +0200

    Create switchToPlayMode.cpp

---
+++ switchToPlayMode.cpp
@@ -0,0 +1,21 @@
+#include "CMUgraphics.h"
+#include <iostream>
+#include 
+void switchToPlayMode(window w){
+    w.SetPen(WHITE,1);
+    w.SetBrush(WHITE);
+    w.DrawRectangle(0,0,w.GetWidth,w.GetHeight,w.GetWidth,w.GetHeight);
+
+    const int count = 5;
+	  const int menuHeight = 80,  menuItemWidth = 50;
+
+	  string MenuIconsPaths[count];	
+	  MenuIconsPaths[0] = "";
+	  MenuIconsPaths[1] = "";
+	  MenuIconsPaths[2] = "";
+	  MenuIconsPaths[3] = "";
+  	MenuIconsPaths[4] = "";
+
+    DrawPlayModeToolBar(w, MenuIconsPaths, count, menuItemWidth, menuHeight)
+}
+
