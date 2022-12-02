54608fa51bb0723bd91f9656ff9b5c88eec136de
amar298sherif <117904287+amar298sherif@users.noreply.github.com>
2022-12-02 23:30:45 +0200

    Update opDeleteShape.cpp

--- operations/opDeleteShape.cpp
+++ operations/opDeleteShape.cpp
@@ -1,1 +1,21 @@
+#include "opDeleteShape.h"
 
+#include "..\Graph.h"
+
+#inclue "..\Shape.h"
+
+#include "..\controller.h"
+
+#include "..\GUI\GUI.h"
+
+opDeleteShape::opDeletShape(controller *pCont):operation(pCont){}
+
+opDeleteShape::~opDeleteShape(){}
+
+void opDeleteShape::Excute(){
+  for(int i =0; i < shapesList.size(); i++){
+      if(shapesList[i]->IsSelected()){
+        shapeList.erase(shapeList.begin() + i);
+      }
+  }
+}
