d0231a05c208c022f2d24cd9fa06ff0c380bb68a
amar298sherif <117904287+amar298sherif@users.noreply.github.com>
2022-12-02 19:19:22 +0200

    Update opDeleteShape.h

--- operations/opDeleteShape.h
+++ operations/opDeleteShape.h
@@ -1,1 +1,9 @@
+#pragma once
+#include "operation.h"
 
+class opDeleteShape : public operation{
+public:
+  opDeleteShape(controller *pCont);
+  virtual ~opDeleteShape();
+  virtual void Excute();
+};
