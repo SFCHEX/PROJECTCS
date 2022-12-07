cfddd60ae77564916db640e273fb0ac4fd7f0035
amar298sherif <117904287+amar298sherif@users.noreply.github.com>
2022-12-07 21:43:03 +0200

    Create opStickImage.cpp

---
+++ operations/opStickImage.cpp
@@ -0,0 +1,17 @@
+#include "opStickImage.h"
+
+#include "..\Shapes\Graph.h"
+
+#include "..\Shapes\Shape.h"
+
+#include "..\controller.h"
+
+#include "..\GUI\GUI.h"
+
+opStickImage::opStickImage(controller * pCont):operation(pCont){}
+
+opStickImage::~opStickImage(){}
+
+void opStickImage::Execute(){
+
+}
