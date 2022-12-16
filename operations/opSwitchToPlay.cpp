0801badd049768b15e16dca1109fefdded720929
CHEX <52354962+SFCHEX@users.noreply.github.com>
2022-12-16 23:24:11 +0200

    Update opSwitchToPlay.cpp

--- operations/opSwitchToPlay.cpp
+++ operations/opSwitchToPlay.cpp
@@ -15,5 +15,14 @@ opSwitchToPlay::~opSwitchToPlay(){}
 void opSwitchToPlay::Execute(){
 		Graph* pGr = pControl->getGraph();
 		GUI* pUI = pControl->GetUI();
+		pUI->PrintMessage("Would you like to save before you enter play mode? [y]yes [anything]no ");
+		if (pUI->GetString()=="y"||pUI->GetString()=="Y"){
+		pUI->PrintMessage("Enter file name to save? ");
+		string file=pUI->GetString();
+	    ofstream myfile;
+	    myfile.open(file+".txt");
+	    pGr->Save(myfile,pUI);
+		}
+
 		pUI->switchToPlay();
 }
