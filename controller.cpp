602cc45b47ec2c3705660b1ea0ed6b5bfdb04b13
CHEX <52354962+SFCHEX@users.noreply.github.com>
2022-12-22 23:24:41 +0200

    removed error

--- controller.cpp
+++ controller.cpp
@@ -5,8 +5,6 @@ controller::controller()
 {
 	pGr = new Graph;
 	pUI = new GUI;	//Create GUI object
-	stack<operation*> UndoStack;
-	stack<operation*> RedoStack;
 }
 
 //==================================================================================//
@@ -51,12 +49,6 @@ operation* controller::createOperation(operationType OpType)
 		case PASTE:
 			pOp = new opPaste(this);
 			break;
-		
-		case DRAG_MODE:
-			pOp = new opDrag(this);
-				break;
-
-
 		case DRAW_SQUARE:
 			pOp = new opAddSquare(this);
 				break;
@@ -92,6 +84,10 @@ operation* controller::createOperation(operationType OpType)
 		case SAVE:
 			pOp = new opSave(this); //create save operation 
 			break;
+
+		case SELECTION_MODE:
+			pOp = new opSelect(this);
+			break;
 		case TO_PLAY:
 			pOp = new opSwitchToPlay(this);
 			break;
@@ -109,8 +105,6 @@ operation* controller::createOperation(operationType OpType)
 			break;
 
 		case STATUS:	//a click on the status bar ==> no operation
-			break;
-		case DO_NOTHING:
 			break;
 
 	}
