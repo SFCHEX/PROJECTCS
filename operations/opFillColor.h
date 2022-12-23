94ac51f8dc813caf15183f9fbe15781462c52e38
CHEX <52354962+SFCHEX@users.noreply.github.com>
2022-12-23 14:07:33 +0200

    did fillcolor

--- operations/opFillColor.h
+++ operations/opFillColor.h
@@ -4,11 +4,19 @@
 
 class opFillColor : public operation
 {
+private:
+bool noPrevColor;
+color newColor;
 
+color previousColor;
+
+shape* selShape=nullptr;
 public:
 	opFillColor(controller* pCont);
 	void Execute();
 	~opFillColor();
+	virtual void Undo();
+	virtual void Redo();
 
 
 };
