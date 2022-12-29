23e450b758f9732b11f3a397de0e7886d9befb46
amar298sherif <117904287+amar298sherif@users.noreply.github.com>
2022-12-30 01:51:57 +0200

    Create opResize.cpp

---
+++ operations/opResize.cpp
@@ -0,0 +1,17 @@
+#include "opResize.h"
+
+#include "..\Shapes\Graph.h"
+
+#include "..\Shapes\Shape.h"
+
+#include "..\controller.h"
+
+#include "..\GUI\GUI.h"
+
+opResize::opResize(controller * pCont):operation(pCont){}
+
+opResize::~opResize(){}
+
+void opResize::Execute(){
+  
+}
