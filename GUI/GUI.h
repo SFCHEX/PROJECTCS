6200fc16ca23a92ebf577d5dc9bf9aa51f90adac
amar298sherif <117904287+amar298sherif@users.noreply.github.com>
2023-01-08 21:23:49 +0200

    Update GUI.h

--- GUI/GUI.h
+++ GUI/GUI.h
@@ -63,7 +63,7 @@ struct GfxInfo	//Graphical info common for all shapes (you may add more members)
 	bool isSelected;	//true if the shape is selected.
 	bool isHidden = false;
 	int HidId;
-	int groupID; //id of the group (not zero if grouped)
+	int groupID =0; //id of the group (not zero if grouped)
 };
 
 
